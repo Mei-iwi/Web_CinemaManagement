0ef6c45511a71a3eae86512a358eb106e04f4971
Phú <phubao13891@gmail.com>
2025-11-14 22:36:05 +0700

    Add ShowtimesContrller

---
+++ Web_CinemaManagement/Controllers/ShowtimesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web_CinemaManagement.Controllers
+{
+    public class ShowtimesController : Controller
+    {
+        // GET: Showtimes
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
