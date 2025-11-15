b0ed965e30dea421a3e1f980aa6513a6aa999b65
Mei-IwI <c2005vn@gmail.com>
2025-11-16 01:32:05 +0700

    Finish TicketManagement Page use Controller and Build API

---
+++ Web_CinemaManagement/Models/ADO/getInfoTickets.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Web_CinemaManagement.Helper;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Web_CinemaManagement.Models.ADO
+{
+    public class getInfoTickets
+    {
+        string str = Helper.ConnectionHelper.getConnectionString("sqlserver", "123456789");
+
+        public List<InfoTickets_Model> getInfo()
+        {
+            List<InfoTickets_Model> ls = new List<InfoTickets_Model>();
+            using (SqlConnection con = new SqlConnection(str))
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM VE", con))
+                {
+                    con.Open();
+
+                    cmd.CommandType = CommandType.Text;
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        InfoTickets_Model info = new InfoTickets_Model();
+
+                        info.mave = reader["MAVE"].ToString();
+                        info.masuat = reader["MASUAT"].ToString();
+                        info.malv = reader["MALV"].ToString();
+                        info.makh = reader["MAKH"].ToString();
+                        info.manv = reader["MANV"].ToString();
+                        info.maghe = reader["MAGHE"].ToString();
+                        info.ngaybanve = DateTime.Parse(reader["NGAYBANVE"].ToString());
+
+                        ls.Add(info);
+                    }
+
+                }
+                return ls;
+            }
+        }
+    }
+}
---
+++ Web_CinemaManagement/wwwroot/js/TicketManagement.js
@@ -0,0 +1,133 @@
+﻿//Khu vực biến
+
+var filter = document.getElementById("Filter");
+
+var button = document.getElementById("submit");
+
+console.log(button)
+//Khu vực hàm
+document.addEventListener("DOMContentLoaded", function () {
+
+    fetch(`/api/tickets/getTickets`)
+        .then(res => {
+            if (!res.ok) {
+                alert("Lỗi kết nối")
+            };
+            return res.json();
+        })
+        .then(data => {
+            console.log(data)
+            const tbody = document.querySelector("#ticketTable tbody");
+
+            tbody.innerHTML = "";
+
+            var index = 1;
+
+            data.forEach(ticket => {
+                const tr = document.createElement("tr");
+                tr.innerHTML = `
+                    <td>${index++}</td>
+                    <td>${ticket.mave}</td>
+                    <td>${ticket.masuat}</td>
+                    <td>${ticket.malv}</td>
+                    <td>${ticket.makh}</td>
+                    <td>${ticket.manv}</td>
+                    <td>${ticket.maghe}</td>
+                    <td>${ticket.ngaybanve}</td>
+                    <td><a href="/Employee/TicketManagement/Details?id=${ticket.mave}">Chi tiết</a></td>
+        `;
+                tbody.appendChild(tr);
+
+
+            });
+
+        })
+        .catch(err => console.error("Lỗi " + err))
+
+});
+
+filter.addEventListener("change", function () {
+
+
+    fetch(`/api/tickets/getIn?time=${this.value}`)
+        .then(res => {
+            if (!res.ok) {
+                alert("Lỗi kết nối");
+            }
+            return res.json();
+        })
+        .then(data => {
+
+            const tbody = document.querySelector("#ticketTable tbody");
+
+            tbody.innerHTML = "";
+
+            var index = 1;
+            data.forEach(ticket => {
+
+                const tr = document.createElement("tr");
+
+                tr.innerHTML = `
+                    <td>${index++}</td>
+                    <td>${ticket.mave}</td>
+                    <td>${ticket.masuat}</td>
+                    <td>${ticket.malv}</td>
+                    <td>${ticket.makh}</td>
+                    <td>${ticket.manv}</td>
+                    <td>${ticket.maghe}</td>
+                    <td>${ticket.ngaybanve}</td>
+                    <td><a href="/Employee/TicketManagement/Details?id=${ticket.mave}">Chi tiết</a></td>
+        `;
+
+                tbody.appendChild(tr);
+
+            });
+
+        })
+        .catch(err => console.error("Lỗi " + err));
+
+});
+
+button.addEventListener("click", function () {
+
+    var text = document.getElementById("Search").value;
+
+    console.log(text)
+    
+    fetch(`/api/tickets/Search?id=${text}`)
+        .then(res => {
+            if (!res.ok) {
+                alert("Lỗi kết nối");
+            }
+            return res.json();
+        })
+        .then(data => {
+
+            const tbody = document.querySelector("#ticketTable tbody");
+
+            tbody.innerHTML = "";
+
+            var index = 1;
+            data.forEach(ticket => {
+
+                const tr = document.createElement("tr");
+
+                tr.innerHTML = `
+                    <td>${index++}</td>
+                    <td>${ticket.mave}</td>
+                    <td>${ticket.masuat}</td>
+                    <td>${ticket.malv}</td>
+                    <td>${ticket.makh}</td>
+                    <td>${ticket.manv}</td>
+                    <td>${ticket.maghe}</td>
+                    <td>${ticket.ngaybanve}</td>
+                    <td><a href="/Employee/TicketManagement/Details?id=${ticket.mave}">Chi tiết</a></td>
+        `;
+
+                tbody.appendChild(tr);
+
+            });
+
+        })
+        .catch(err => console.error("Lỗi " + err));
+});
