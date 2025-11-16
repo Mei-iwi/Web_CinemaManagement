b694d5e4e915e47572eb7e9219abd049c0c7db68
Linh <luuduclinh007@gmail.com>
2025-11-16 20:34:37 +0700

    Services and Reports

---
+++ Web_CinemaManagement/Models/Report/ReportViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web_CinemaManagement.Models.Report
+{
+    public class ReportViewModel
+    {
+        public DateTime? FromDate { get; set; }
+        public DateTime? ToDate { get; set; }
+        public string Mode { get; set; }
+
+        public int TotalTickets { get; set; }
+        public int TotalServiceItems { get; set; }
+        public decimal TicketRevenue { get; set; }
+        public decimal ServiceRevenue { get; set; }
+        public decimal TotalRevenue { get; set; }
+
+        public List<ReportItem> Items { get; set; }
+    }
+
+    public class ReportItem
+    {
+        public string Label { get; set; }
+        public decimal Revenue { get; set; }
+    }
+}
