b9382a50952a6e425df7f012e5729807dc4cc966
Mei-IwI <c2005vn@gmail.com>
2025-11-17 15:00:34 +0700

    Add animation for login

--- Web_CinemaManagement/wwwroot/js/Login.js
+++ Web_CinemaManagement/wwwroot/js/Login.js
@@ -5,6 +5,16 @@ var pass = document.getElementById("Password");
 
 var form = document.querySelector(".form-login");
 
+var formLogin = document.querySelector("form");
+
+var err = document.querySelector(".validation-summary-errors");
+
+var text = document.querySelector("h4[id='start']")
+
+let intervalId = null;
+
+console.log(form);
+
 //Khu vực hàm, xử lý
 checkbox.addEventListener("change", function (e) {
     e.preventDefault();
@@ -24,3 +34,70 @@ form.addEventListener("click", function () {
     this.classList.add("active");
 });
 
+function startInterval() {
+
+    if (intervalId !== null) return;
+
+
+    intervalId = setInterval(() => {
+        if (text.innerHTML === "ĐĂNG NHẬP TẠI ĐÂY") {
+            text.innerHTML = "ĐĂNG NHẬP THẤT BẠI";
+            text.classList.remove("text");
+            text.style.color = "yellow"
+
+        } else {
+            text.innerHTML = "ĐĂNG NHẬP TẠI ĐÂY";
+            text.style.color = ""
+            text.classList.add("text");
+
+        }
+
+        form.classList.toggle("MessageErr");
+    }, 2000);
+
+
+};
+
+function startIntervalLogin() {
+
+    if (intervalId !== null) return;
+
+
+    intervalId = setInterval(() => {
+        form.classList.toggle("MessageLogin");
+    }, 2000);
+
+
+};
+
+
+function stopInterval() {
+    clearInterval(intervalId);
+    intervalId = null;
+    form.classList.remove("MessageErr");
+    text.innerHTML = "ĐĂNG NHẬP TẠI ĐÂY";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    if (err !== null) {
+        startInterval();
+    } else {
+        startIntervalLogin();
+    }
+});
+
+
+form.addEventListener("mouseover", function () {
+    stopInterval();
+    form.classList.remove("MessageErr", "MessageErrHide");
+});
+
+form.addEventListener("mouseout", function () {
+    startInterval();
+});
+
+
+form.onclick = () => {
+    stopInterval();
+    form.classList.remove("MessageErr", "MessageErrHide");
+}
